1ea173fd4bbea95c713e6112cefb3cfd1924931b
kianibound <kianibound@gmail.com>
2023-07-10 23:28:57 +0330

    add useContext to all comps

--- src/App.js
+++ src/App.js
@@ -63,8 +63,8 @@ function App() {
         </button>
 
         <Header />
-        <Main posts={searchedPosts} onAddPost={handleAddPost} />
-        <Archive onAddPost={handleAddPost} />
+        <Main />
+        <Archive />
         <Footer />
       </section>
     </PostContext.Provider>
@@ -103,7 +103,8 @@ function Results() {
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-function Main({ posts, onAddPost }) {
+function Main() {
+  const { posts, onAddPost } = useContext(PostContext);
   return (
     <main>
       <FormAddPost onAddPost={onAddPost} />
@@ -112,7 +113,8 @@ function Main({ posts, onAddPost }) {
   );
 }
 
-function Posts({ posts }) {
+function Posts() {
+  const {posts} = useContext(PostContext)
   return (
     <section>
       <List posts={posts} />
@@ -120,9 +122,11 @@ function Posts({ posts }) {
   );
 }
 
-function FormAddPost({ onAddPost }) {
+function FormAddPost() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
+
+  const {onAddPost} = useContext(PostContext)
 
   const handleSubmit = function (e) {
     e.preventDefault();
@@ -149,7 +153,8 @@ function FormAddPost({ onAddPost }) {
   );
 }
 
-function List({ posts }) {
+function List() {
+  const {posts} = useContext(PostContext)
   return (
     <ul>
       {posts.map((post, i) => (
@@ -162,7 +167,8 @@ function List({ posts }) {
   );
 }
 
-function Archive({ onAddPost }) {
+function Archive() {
+  const { onAddPost } = useContext(PostContext);
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
